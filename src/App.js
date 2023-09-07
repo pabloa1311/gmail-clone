9a09a7e50e893ca59533621d74011f4d75c5e853
Pablo Andres Ramirez Lopez <pabloa1311@gmail.com>
2023-09-07 15:36:05 -0500

    Sidebar Component Created and options in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <SideBar/>
     </div>
   );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import "../styles/SideBar.css"
+import React from "react";
+import "../styles/SideBar.css";
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from '@mui/icons-material/Inbox';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
+
 
 function SideBar() {
   return (
-    <div>SideBar</div>
-  )
+    <div className="sidebar">
+      <Button startIcon={<AddIcon fontSize="large"  />} className="sidebar__compose">Compose</Button>
+
+      <SidebarOption Icon={InboxIcon} title={"Inbox"} number={54} selected={true}/>
+      <SidebarOption Icon={StarBorderIcon} title={"Starred"} number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title={"Snoozed"} number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title={"Important"} number={54} />
+      <SidebarOption Icon={SendOutlinedIcon} title={"Sent"} number={54} />
+      <SidebarOption Icon={InsertDriveFileOutlinedIcon} title={"Drafts"} number={54} />
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
